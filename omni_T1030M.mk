6be38b4d9b033cf72dbe79462171e138fe0d46f1
Momo5418 <momo5418@mocn.top>
2024-09-17 23:47:32 +0800

    更新 omni_T1030M.mk

--- omni_T1030M.mk
+++ omni_T1030M.mk
@@ -9,9 +9,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-
 # Inherit from T1030M device
 $(call inherit-product, device/alldocube/T1030M/device.mk)
 
